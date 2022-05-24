4f1d17b787796228846edd11548aae767708f859
dibhardwaj <dikshit.bhardwaj@soprasteria.com>
2022-05-24 22:58:07 +0530

    initial

---
+++ src/jenkinsTest/testfibonnaci.java
@@ -0,0 +1,21 @@
+package jenkinsTest;
+
+public class testfibonnaci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		int i = 0,j=1,num=10;
+		
+		for(int k = 0;k<num ;k++) {
+			int swap = i + j ;
+			System.out.println(swap);
+			i = j;
+			j= swap;
+			
+		}
+
+	}
+
+}
